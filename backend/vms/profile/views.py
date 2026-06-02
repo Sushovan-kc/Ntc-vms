bf4724ce5bf0e84ac8f0d983492dfa96861d1cf5
Sushovan-Kc <sushovankc7@gmail.com>
2026-06-02 12:25:28 +0545

    first upload

---
+++ backend/vms/profile/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import UserProfile
+# Create your views here.
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
